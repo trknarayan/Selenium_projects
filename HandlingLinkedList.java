b1d616e467a293c1f76c074e02861798a1d6eb43
trknarayan <42097023+trknarayan@users.noreply.github.com>
2018-08-04 22:52:20 +0530

    added 58

--- HandlingLinkedList.java
+++ HandlingLinkedList.java
@@ -2,7 +2,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
- * 
+ *
  */
 
 /**
@@ -16,24 +16,24 @@ public class HandlingLinkedList {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
+
+
+
 		LinkedList<Integer> a = new LinkedList<Integer>();
-		
+
 		a.add(1);
 		a.add(11);
 		a.add(12);
 		a.add(13);
-		a.add(14);
-		
+		a.add(58);
+
 		System.out.println("Using Iterator");
 		Iterator<Integer> it = a.iterator();
 		while (it.hasNext()) {
 			System.out.println(it.next());
 		}
 
-		
+
 
 	}
 
